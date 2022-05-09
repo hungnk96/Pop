4d0c1148c75e1cf786779b88e6b1aa43ea56249a
hungnk96 <hungnk1996@gmail.com>
2022-05-09 18:01:20 +0700

    update

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ mapboxgl.accessToken = 'pk.eyJ1IjoicmV2b3RlY2giLCJhIjoiY2swZGJobWg3MDY2NzNtbnVwa
 const map = new mapboxgl.Map({
     container: 'map',
     style: 'mapbox://styles/mapbox/light-v10',
-    center: [-103.5917, 40.6699],
+    center: [107.5917, 21.6699],
     zoom: 3
 });
 
@@ -27,19 +27,15 @@ map.on('load', () => {
         source: 'earthquakes',
         filter: ['has', 'point_count'],
         paint: {
-            // Use step expressions (https://docs.mapbox.com/mapbox-gl-js/style-spec/#expressions-step)
-            // with three steps to implement three types of circles:
-            //   * Blue, 20px circles when point count is less than 100
-            //   * Yellow, 30px circles when point count is between 100 and 750
-            //   * Pink, 40px circles when point count is greater than or equal to 750
+        
             'circle-color': [
                 'step',
                 ['get', 'point_count'],
-                '#51bbd6',
+                '#00ac78',
                 100,
-                '#f1f075',
+                '#FFD05C',
                 750,
-                '#f28cb1'
+                '#FF4123'
             ],
             'circle-radius': [
                 'step',
@@ -62,6 +58,9 @@ map.on('load', () => {
             'text-field': '{point_count_abbreviated}',
             'text-font': ['DIN Offc Pro Medium', 'Arial Unicode MS Bold'],
             'text-size': 12
+        },
+        paint: {
+            "text-color": "#ffffff"
         }
     });
 
@@ -71,7 +70,7 @@ map.on('load', () => {
         source: 'earthquakes',
         filter: ['!', ['has', 'point_count']],
         paint: {
-            'circle-color': '#11b4da',
+            'circle-color': '#00ac78',
             'circle-radius': 4,
             'circle-stroke-width': 1,
             'circle-stroke-color': '#fff'
